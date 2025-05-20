b87cd01486de4c7aa88f7a203df5db765e1cfb06
Kamila Januszewicz <19281966+KamJan@users.noreply.github.com>
2025-05-20 13:38:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,5 @@ if __name__ == "__main__":
         build_embeddings()
         print("Embeddings saved.")
     else:
-        app.run(port=5000)
+        port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port) 
